2bee54531970e3ced93ab0c429ce888f03bddf29
Vega <125924992+Vega863@users.noreply.github.com>
2023-02-20 13:06:14 -0900

    Update Console-Log.py

--- Console-Log.py
+++ Console-Log.py
@@ -6,7 +6,6 @@ def log():
     date = datetime.datetime.now()
     if os.path.exists("log.txt") == False:
         with open("log.txt", "w") as file:
-            # [Info  - 22:50:59.240] Starting TS Server
             file.write(f"[Date - {date} ] Connexion User Sessions : 0 ")
     with open("log.txt", "r") as file:
         contents = file.read()
