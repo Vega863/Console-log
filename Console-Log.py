8cf86dcac81e86668ce8ee0750134996c3a52d90
Vega <125924992+Vega863@users.noreply.github.com>
2023-02-20 13:05:52 -0900

    Add files via upload

---
+++ Console-Log.py
@@ -0,0 +1,28 @@
+import os
+import datetime
+
+
+def log():
+    date = datetime.datetime.now()
+    if os.path.exists("log.txt") == False:
+        with open("log.txt", "w") as file:
+            # [Info  - 22:50:59.240] Starting TS Server
+            file.write(f"[Date - {date} ] Connexion User Sessions : 0 ")
+    with open("log.txt", "r") as file:
+        contents = file.read()
+        try:
+            num = contents
+            num = num.split(" ")[-1]
+            num = int(num)
+        except ValueError:
+            num = 0
+    with open("log.txt", "a") as file:
+        num += 1
+        num = str(num)
+        file.write(
+            str(f'\n[Date  -{date} ] Connexion User Sessions : {num}'))
+        
+
+
+if __name__ == "__main__":
+    log()
